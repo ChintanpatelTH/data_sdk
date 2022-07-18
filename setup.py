44bf9b9e15ef03ae1f48bd5c1fed05b052350303
Chintan Patel <chintan.patel@techolding.co>
2022-07-18 12:53:47 +0000

    Bump version: 2.0.0 → 2.0.1

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ChintanpatelTH/data_sdk',
-    version='2.0.0',
+    version='2.0.1',
     zip_safe=False,
 )
